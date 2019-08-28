047f2b2aa2288ee309abfe1ad182e1394b5d0e13
Camilo Torres - Analista De Inteligencia De Negocios <ctorrest@clinimed.local>
2019-08-28 10:22:50 -0500

    version inicial ok

--- src/main/java/country/local/Reporting/ReportingApplication.java
+++ src/main/java/country/local/Reporting/ReportingApplication.java
@@ -1,12 +1,19 @@
-package country.local.reporting;
+package country.local.Reporting;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("country.local.Reporting.dao")
-public class ReportingApplication {
+public class ReportingApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ReportingApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ReportingApplication.class, args);
